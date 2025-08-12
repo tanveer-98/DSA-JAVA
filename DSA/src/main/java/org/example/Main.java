41938b542bbaad431a0cd3be9bab1e669c538ba2
tanveer-98 <tnvrahmed98@gmail.com>
2025-08-12 22:28:16 +0530

    Intellij Test commit

---
+++ DSA/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import graphs.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Car car = new Car();
+        System.out.println(car);
+    }
+}
